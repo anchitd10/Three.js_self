69374a7ac14d520b012d2c498eb24e0b26e3a21d
Anchit Das <anchitd10@gmail.com>
2025-03-16 22:08:08 +0530

    files added

---
+++ Dynamic Sphere (Perlin Noise)/src/js/main.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background.
+// renderer.setClearColor(0xfefefe);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Sets orbit control to move the camera around.
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// Camera positioning.
+camera.position.set(6, 8, 14);
+orbit.update();
+
+const uniforms = {
+    u_resolution: {type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight)},
+    u_time: {type: 'f', value: 0.0}
+};
+
+const mat = new THREE.ShaderMaterial({
+    wireframe: true,
+    uniforms,
+    vertexShader: document.getElementById('vertexshader').textContent,
+    fragmentShader: document.getElementById('fragmentshader').textContent,
+});
+
+const geo = new THREE.IcosahedronGeometry(4, 30);
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+mesh.material.wireframe = true;
+
+const clock = new THREE.Clock();
+function animate() {
+    // const target = controls1.target;
+    // controls1.update();
+    // controls2.target.set(target.x,target.y, target.z);
+    // controls2.update();
+
+    uniforms.u_time.value = clock.getElapsedTime();
+    renderer.render(scene, camera);
+
+    mesh.rotation.x = uniforms.u_time.value * 0.0003;
+    mesh.rotation.z = uniforms.u_time.value * 0.0003;
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
